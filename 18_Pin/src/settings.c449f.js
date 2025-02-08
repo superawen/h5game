b000cd2450b78626f7f386e5e6b9c50365b4f66f
superawen <superawen@qq.com>
2025-02-08 10:10:01 +0800

    h5-pin

---
+++ 18_Pin/src/settings.c449f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","earth","small"],collisionMatrix:[[false],[false,false,false],[false,false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"c870e",main:"5d07a"}};
