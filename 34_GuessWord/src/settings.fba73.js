19111b80edd2fae5cb76736f26910cc0145f8f35
superawen <superawen@qq.com>
2025-04-16 23:27:19 +0800

    看图猜成语

---
+++ 34_GuessWord/src/settings.fba73.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"f1c61",main:"13c05"}};
