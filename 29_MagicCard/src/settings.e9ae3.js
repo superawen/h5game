3b623854ae5a119f8f552bf937555c60b702268c
superawen <superawen@qq.com>
2025-04-23 23:08:08 +0800

    魔法卡牌

---
+++ 29_MagicCard/src/settings.e9ae3.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/game.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"71a50",main:"f844d"}};
