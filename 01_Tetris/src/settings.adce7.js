1b335ffbf86e0b94392fbef6be7207295b51a89b
superawen <superawen@qq.com>
2025-04-26 21:04:30 +0800

    俄罗斯方块

---
+++ 01_Tetris/src/settings.adce7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/game.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"08206",main:"b2776"}};
