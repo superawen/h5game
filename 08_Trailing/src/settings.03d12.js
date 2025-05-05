f2b4a33d2a1d607de8f40ae6bafdc1c20ae018aa
superawen <superawen@qq.com>
2025-05-05 20:25:01 +0800

    拖尾残影

---
+++ 08_Trailing/src/settings.03d12.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"be32f",main:"4eea2"}};
