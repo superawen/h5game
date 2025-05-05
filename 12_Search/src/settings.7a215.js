f498f1527d3bb20c0420f9833598d0620fced451
superawen <superawen@qq.com>
2025-05-05 21:20:54 +0800

    火眼金睛不服来战

---
+++ 12_Search/src/settings.7a215.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"3ea3b",main:"f28d2"}};
