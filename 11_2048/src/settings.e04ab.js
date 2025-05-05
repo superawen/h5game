8c93ebf0e2cc170aa0ba660ce526e5978a5ca195
superawen <superawen@qq.com>
2025-05-05 21:09:13 +0800

    2048

---
+++ 11_2048/src/settings.e04ab.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"b4209",main:"03e6f"}};
