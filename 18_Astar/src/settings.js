71e9c24c0af0722bea6ccdfe6f71a5bfdda354dc
superawen <superawen@qq.com>
2025-05-11 21:35:25 +0800

    AStar+TileMap

---
+++ 18_Astar/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/map0.fire",orientation:"portrait",jsList:[]};
