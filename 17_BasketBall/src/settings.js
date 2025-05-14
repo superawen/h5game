e90491ef8aebd9b8c1526af245437dbda3b6b47f
superawen <superawen@qq.com>
2025-05-14 23:15:40 +0800

    激情篮球

---
+++ 17_BasketBall/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ball","box"],collisionMatrix:[[false,false,false],[false,false,true],[false,true,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Game.fire",orientation:"",jsList:[]};
