a6d58b447b3f3d63241c1eff2fb6b6aa49186f34
superawen <superawen@qq.com>
2025-06-02 19:02:22 +0800

    一撮金币飞行动画

---
+++ 10_FlyCoin/src/settings.f595f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/menu.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"f9928",main:"67c43"}};
