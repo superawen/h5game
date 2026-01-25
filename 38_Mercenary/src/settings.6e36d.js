15bd89d3b4d4ea5e3636b3e58881a998a1ad6a93
superawen <superawen@qq.com>
2026-01-25 21:51:32 +0800

    塔防英雄

---
+++ 38_Mercenary/src/settings.6e36d.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/LoadScene.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",resources:"6f45a",main:"95797"}};
