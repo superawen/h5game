e033d3908b52549e23e5eaba279e0c408a09e693
superawen <superawen@qq.com>
2026-02-28 20:57:25 +0800

    大舅爱消除

---
+++ 10_ClearCube2/src/settings.0d969.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"b6329",main:"739a8"}};
