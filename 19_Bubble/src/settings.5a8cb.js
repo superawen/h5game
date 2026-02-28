a814381006db5d929b73e3487f8e8d9a16a24e35
superawen <superawen@qq.com>
2026-02-28 20:14:12 +0800

    无敌泡泡龙

---
+++ 19_Bubble/src/settings.5a8cb.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"66864",main:"98c26"}};
