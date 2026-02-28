588020d6da40e5b145cd9e622b673ef03b2ef8ad
superawen <superawen@qq.com>
2026-02-28 20:09:26 +0800

    糖果消消乐

---
+++ 17_Jelly/src/settings.4869b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"847f2",main:"8faaf"}};
