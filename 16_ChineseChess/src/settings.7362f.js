217c0697ebe63f36448c5271cae47e3e8f1d9179
superawen <superawen@qq.com>
2026-02-28 21:02:56 +0800

    中国象棋

---
+++ 16_ChineseChess/src/settings.7362f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Game.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"f5c76",main:"75562"}};
