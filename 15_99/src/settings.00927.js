10b1785be21d2d1bdffd49bcbe004f2a93eaf730
superawen <superawen@qq.com>
2026-02-28 21:11:26 +0800

    99

---
+++ 15_99/src/settings.00927.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"a19f1",main:"b4df5"}};
